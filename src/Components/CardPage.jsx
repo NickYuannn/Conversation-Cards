921fd22f3b2be9dcee3f423a148dbfcd893da93c
Nick Y <nk.yuan20052gmail.com>
2024-08-01 22:45:57 -0400

    gemini

--- src/Components/CardPage.jsx
+++ src/Components/CardPage.jsx
@@ -2,20 +2,40 @@ import React, { useState } from "react";
 import "./CardPage.css";
 import { FaQuestion } from "react-icons/fa";
 import Footer from "./Footer";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 function CardPage(props) {
-  const [randomPrompt, setRandomPrompt] = useState(
-    props.prompts[Math.floor(Math.random() * props.prompts.length)]
-  );
+  const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+
+  const genAI = new GoogleGenerativeAI(apiKey);
+
+  const [randomPrompt, setRandomPrompt] = useState("first one");
+
+  const [theme, setTheme] = useState("");
+
+  async function generatePrompt() {
+    try {
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+      const prompt = `Generate an interesting and deep question to start a conversation for ${
+        props.packname
+      } under 10 words no emojis${theme ? ` with the theme ${theme}` : ""}.`;
+      console.log(prompt);
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = await response.text();
+      setRandomPrompt(text);
+      console.log(text);
+    } catch (error) {
+      console.error("Error generating prompt:", error);
+      setRandomPrompt("Error due to safety filter, try again");
+    }
+  }
 
   const [reveal, setReveal] = useState(false);
 
   const [turns, setTurns] = useState("");
 
   function newPrompt() {
-    setRandomPrompt(
-      props.prompts[Math.floor(Math.random() * props.prompts.length)]
-    );
     setTurns("");
     setReveal(false);
   }
@@ -36,13 +56,17 @@ function CardPage(props) {
     }, randomSeconds * 1000);
   }
 
-  function revealPrompt() {
+  async function revealPrompt() {
+    await generatePrompt();
     setReveal(true);
   }
   return (
     <div className="cardpage-container">
       <h1>{props.packname}</h1>
-      <div className="conversation-card-container" onClick={revealPrompt}>
+      <div
+        className="conversation-card-container"
+        onClick={reveal ? null : revealPrompt}
+      >
         {reveal ? (
           <>
             <h2>{randomPrompt}</h2>
@@ -54,6 +78,19 @@ function CardPage(props) {
             <FaQuestion className="question-icon" />
           </>
         )}
+      </div>
+
+      <div className="theme-container">
+        <h2>Have something in common?</h2>
+        <div className="theme-input">
+          <input
+            type="text"
+            placeholder="Center the questions on a certain topic!"
+            onChange={(e) => {
+              setTheme(e.target.value);
+            }}
+          />
+        </div>
       </div>
       <button onClick={newPrompt}>New Prompt</button>
 
